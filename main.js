5f5a66867c72b839119be170814d65e91614849f
shohamBus <shohambuskila@gmail.com>
2022-05-27 15:38:52 +0300

    try to adding a counter

--- main.js
+++ main.js
@@ -26,47 +26,48 @@ const front = document.querySelectorAll(".front");
 const back = document.querySelectorAll(".back");
 let cardsChosen = [];
 let cardsWonArr = [];
-let counter=5;
+let counter=10;
 let lives=document.querySelector("h2 span");
-lives.innerText=counter
+// lives.innerText=counter;
 
-if (counter===0){
-  alert("Game over lets try again")
-  displayFront()
-}
-// updateCounter(counter)
-// function updateCounter(counter){
-//   counter--
-// }
-// console.log(counter)
+// if (counter===0){
+  //   alert("Game over lets try again")
+  //   displayFront()
+  // }
+  // updateCounter(counter)
 
-//randomize the array
-
-shuffleArray(cardsArray)
-function shuffleArray(array) {
-  array.sort( () => .5 - Math.random() );
-}
-
-//insert emoji to the cards
-
-function displayFront() {
-  for (let i = 0; i < cardsArray.length; i++) {
-    back[i].innerHTML = cardsArray[i];
+  //randomize the array
+  
+  shuffleArray(cardsArray)
+  function shuffleArray(array) {
+    array.sort( () => .5 - Math.random() );
   }
-}
-displayFront();
+  
+  //insert emoji to the cards
+  
+  function displayFront() {
+    for (let i = 0; i < cardsArray.length; i++) {
+      back[i].innerHTML = cardsArray[i];
+    }
+  }
+  displayFront();
+  
+  //flip card
+  
+  for(const card of cards){
+    card.addEventListener("click", flipCard);
+  }
 
-//flip card
+  console.log(counter);
 
-for(const card of cards){
-  card.addEventListener("click", flipCard);
-}
-function flipCard() {
+  function flipCard(counter) {
   this.classList.toggle("flipped");
   cardsChosen.push(this)
+  counter--;
   if (cardsChosen.length===2){
     setTimeout(function(){check(cardsChosen)},500)
   }
+  return counter
 }
 
 //check if match
@@ -76,15 +77,20 @@ function check(arr) {
     let back=document.getElementsByClassName(arr[0].lastElementChild.classList[1])
     back[0].style.background='rgb(171,220,77)';
     back=document.getElementsByClassName(arr[1].lastElementChild.classList[1])
-      back[0].style.background='rgb(171,220,77)';
-        cardsWonArr.push(arr[0],arr[1]);
-      }else{
-        arr.forEach(element => {
-          element.classList.toggle("flipped")
-        });
-        // counter--;
-      }
-      cardsChosen=[];
-     
-    }
-    
+    back[0].style.background='rgb(171,220,77)';
+    cardsWonArr.push(arr[0],arr[1]);
+    // cardsWon(cardsWonArr);
+  }else{
+    arr.forEach(element => {
+      element.classList.toggle("flipped")
+    });
+  }
+  cardsChosen=[];
+}
+
+// function cardsWon(arr){
+//   for (const item of arr){
+//    item = document.getElementsByClassName('inner');
+//     item.remove();
+//     }
+//   }
